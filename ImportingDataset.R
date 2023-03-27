9e51a460cc8da5a3efac2cfb3aa2ecb6dc8230cc
AnceTetere <ance.tetere@outlook.com>
2023-03-27 16:30:58 +0300

    Updated file: ImportingDataset.R

--- ImportingDataset.R
+++ ImportingDataset.R
@@ -2,12 +2,13 @@
 stats <- read.csv(file.choose())
 stats
 
-#Method 2: Set WD and read in data from there
-getwd()
-setwd("D:\\UDEMY TRAINING\\R-PROGRAMMING\\R-DemographicAnalyses")
-getwd()
-
 rm(stats)
 
+#Method 2: Set WD and read in data from there
+getwd()
+setwd("D:\\R-PROGRAMMING\\R-DemographicAnalyses")
+getwd()
+
 stats <- read.csv("DemographicData.csv")
+stats$Income.Group <- as.factor(stats$Income.Group)
 stats
